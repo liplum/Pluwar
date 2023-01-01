aa33fd2aa1fa98cb42dd968aa4c4951540ad3b47
Liplum <Li_plum@outlook.com>
2023-01-01 17:41:42 +0800

    Skill and Elf.

---
+++ backend/game/entity.py
@@ -0,0 +1,27 @@
+class Skill:
+    pass
+
+
+EmptySkill = Skill()
+
+
+class Elf:
+    def __init__(self):
+        self.name = "Default Elf"
+        self.description = "No description."
+
+
+EmptyElf = Elf()
+
+
+class SkillEntity:
+    def __init__(self):
+        self.type: Skill = EmptySkill
+        self.availableCount = 0
+
+
+class ElfEntity:
+    def __init__(self):
+        self.type = EmptyElf
+        self.hp = 0
+        self.skillSlots = []
