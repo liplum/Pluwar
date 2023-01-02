486bff788ecf6481a1c9a491e3b825789cb0ffb9
Liplum <Li_plum@outlook.com>
2023-01-02 15:49:15 +0800

    Channel

--- backend/foundation.py
+++ backend/foundation.py
@@ -5,3 +5,26 @@ from typing import Protocol, runtime_checkable
 class PayloadConvertible(Protocol):
     def toPayload(self) -> dict:
         pass
+
+
+@runtime_checkable
+class Channel(Protocol):
+    name: str
+
+    async def onMessage(self, json):
+        pass
+
+
+class ChannelDispatcher:
+    def __init__(self):
+        self.name2Channel = {}
+
+    def registerChannel(self, channel: Channel):
+        self.name2Channel[channel.name] = channel
+
+    async def onMessage(self, json):
+        channelName = json["channel"]
+        if channelName in self.name2Channel:
+            channel = self.name2Channel[channelName]
+            data = json["data"]
+            channel.onMessage(data)
--- backend/server.py
+++
@@ -1,8 +0,0 @@
-class Datapack:
-    def __init__(self):
-        self.channel = ""
-        self.data = None
-
-    def fromJson(self, json):
-        self.channel = json["channel"]
-        self.data = json["data"]
