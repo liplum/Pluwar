e5756f435f25403162ed05ac8f858c07885dca48
Liplum <Li_plum@outlook.com>
2023-01-02 13:42:55 +0800

    empty implementation.

--- backend/loop.py
+++ backend/loop.py
@@ -1,12 +1,44 @@
-async def gameLoop(ctx):
-    turnNumber = 0
+class GameLoopContext:
+    def __init__(self):
+        self.turn = 0
+
+    async def goNextTurn(self):
+        self.turn += 1
+
+    async def checkGameEnd(self) -> bool:
+        pass
+
+    async def judgeActionOrder(self) -> tuple["ActorPlayer", "ActorPlayer"]:
+        pass
+
+    async def settleAction(self, action: "GameAction"):
+        pass
+
+    async def onGameEnd(self):
+        pass
+
+
+class ActorPlayer:
+
+    async def awaitAction(self, ctx: GameLoopContext) -> "GameAction":
+        """
+        If timeout, select the first available action
+        """
+        pass
+
+
+class GameAction:
+    pass
+
+
+async def gameLoop(ctx: GameLoopContext):
     while await ctx.checkGameEnd():
         first, second = await ctx.judgeActionOrder()
         # Action from first player
-        firstAction = await first.awaitAction(ctx)  # if timeout, select the first available action
+        firstAction = await first.awaitAction(ctx)
         await ctx.settleAction(firstAction)
         # Action from second player
         secondAction = await second.awaitAction(ctx)
         await ctx.settleAction(secondAction)
-        turnNumber += 1
+        await ctx.goNextTurn()
     await ctx.onGameEnd()
